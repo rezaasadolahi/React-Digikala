69070979cf62a960b106457b9da7af78bb464685
Reza <rezaasadollahi0021@gmail.com>
2022-01-15 18:15:25 +0100

    Design part of the site

---
+++ src/Components/Profile/DATA/DATA_USER.js
@@ -0,0 +1,18 @@
+
+
+const DATA_USER = [
+    {
+        "Full_name": "رضا اسداللهی دهکردی",
+        "Email_Address": "rezaasadollahi0021@gmail.com",
+        "valet": 995,
+        "phone": '0933000111',
+    },
+    {
+        "Full_name": "محمد اسداللهی دهکردی",
+        "Email_Address": "mohamadasadolahi52@gmail.com",
+        "valet": 189,
+        "phone": '093444222',
+    },
+]
+
+export default DATA_USER
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 //* image
 import inviteFriend from './Image/InviteFriend.jpg'
+import Icon_Profile from './Image/profile.svg'
 //* CSS
 import './CSS/Profile.css'
+//* DATA
+import DATA_USER from './DATA/DATA_USER'
+
 
 
 
 
 
 function Profile() {
+    const [data, setData] = useState('')
+    const Email = useSelector(state => state.Email.EmailUser)
+
+
+    useEffect(() => {
+        // Etelate emaili ke sabt shode moqe vorud ya sabtenam va Emaily ke moqe vorud mizanim ra migirim
+        let newSetData = DATA_USER.filter(item => item.Email_Address === Email).find(item => item)
+        setData(newSetData)
+    }, [data])
+
+    
+
+
     return (
         <div id='ManageProfile'>
             <section className='box1'>
@@ -16,9 +34,27 @@ function Profile() {
 
             <section className='box2'>
                 <img src={inviteFriend} alt="" />
-                <section id='information-account'>
+                <div id='information-account'>
+                    <section id='account_name'>
+                        <section id='right'>
+                            <img src={Icon_Profile} alt="" id='icon-profile' />
+                            <section id='Vertical_1'></section>
+                            <section id='Vertical_2'></section>
+                        </section>
+                        <section id='left'>
+                            <section id='fullName'>
+                                <p>{data.Full_name}</p>
+                                <p>{data.phone}</p>
+                            </section>
+                            <section id='valet'>
 
-                </section>
+                            </section>
+                        </section>
+                    </section>
+
+
+
+                </div>
             </section>
         </div>
     )
