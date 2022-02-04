d4937e25ed19d63174e1b1bca64919be68265e43
Reza <rezaasadollahi0021@gmail.com>
2022-02-04 06:01:20 -0800

    End CompareProduct

--- src/Components/Compareproducts/Compareproducts.jsx
+++ src/Components/Compareproducts/Compareproducts.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-
+//* Icon
+import { AiOutlineCloseCircle } from "react-icons/ai"
 //* CSS
 import './CSS/Compareproducts.css'
 
 
 
+
+
+
+
 function Compareproducts() {
   const [modal, setModal] = useState(false)
-
+  const [infoItem, setInfoItem] = useState('')
   const ProductDetail = useSelector(state => state.GetProductDetail.Product)
   const DataProduct = useSelector(state => state.Get_Data_Product.DataProduct)
 
   //* Modal Select Product for Compare
   const handleModal = () => setModal(!modal)
 
+  //* When click on item in modal box
+  const handleItemInModal = (item) => {
+    setInfoItem(item)
+    setModal(!modal)
+  }
 
 
 
@@ -32,28 +42,44 @@ function Compareproducts() {
           </section>
           {/* Box 2 */}
           <section id='box2'>
-            <section id='ManageModal'>
-              <section id='selectProduct' onClick={handleModal}>Icon: Open modal</section>
-
-              <section id={modal ? 'boxModal-on' : 'boxModal-off'}>
-                <h2 id={modal ? 'icon-closeBoxModal-on' : 'icon-closeBoxModal-off'} onClick={handleModal}>X</h2>
-                <section className={modal ? 'modal-on' : 'modal-off'}>
-                  {
-                    DataProduct.map(item => {
-                      return (
-                        <div key={item.id} className="mt-5 mb-5">
-                          <img src={item.imgsrc} alt="Image Producs" width="250" height="250" />
-                        </div>
-                      )
-                    })
-                  }
+            {
+              infoItem === '' ?
+                <section id='ManageModal'>
+                  <section id='selectProduct' onClick={handleModal}>Icon: Open modal</section>
+                  <section id={modal ? 'boxModal-on' : 'boxModal-off'}>
+                    <AiOutlineCloseCircle
+                      id={modal ? 'icon-closeBoxModal-on' : 'icon-closeBoxModal-off'}
+                      onClick={handleModal}
+                    />
+                    <section className={modal ? 'modal-on' : 'modal-off'}>
+                      {
+                        DataProduct.map(item => {
+                          return (
+                            <div key={item.id} onClick={() => handleItemInModal(item)} id="items" className="mt-5 mb-5">
+                              <img src={item.imgsrc} alt="Image Producs" width="200" height="200" />
+                              <p>{item.MoshaKhasat_EN}</p>
+                            </div>
+                          )
+                        })
+                      }
+                    </section>
+                  </section>
                 </section>
-              </section>
-
-            </section>
-
-
-
+                :
+                null
+            }
+            {
+              infoItem !== '' ?
+                <section>
+                  <img src={infoItem.imgsrc} alt="image Product" width="200" height="200" />
+                  <ul className='list-group'>
+                    <li className='list-group-item mt-1'>{infoItem.MoshaKhasat_EN}</li>
+                    <li className='list-group-item'>{infoItem.MoshakhasatKamel}</li>
+                  </ul>
+                </section>
+                :
+                null
+            }
           </section>
         </section>
       </div>
